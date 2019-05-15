8f8820eb2e5fa27ef6f658b242b7c598cc9ee929
marinamontelo <marinamontelo@gmail.com>
2019-05-15 08:41:55 -0300

    teste

--- src/main/java/com/teste/extract/ExtractFile.java
+++ src/main/java/com/teste/extract/ExtractFile.java
@@ -1,14 +1,20 @@
 package com.teste.extract;
+
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
 public class ExtractFile {
+
 	static final int linhasPorVez = 3;
+	static final HashMap<String, Integer> numerosMap = new HashMap<String, Integer>();
+	static final HashMap<Integer, String> patternMap = new HashMap<Integer, String>();
 
 	public static String obtemFragmentoDigito(String linha, int startIndex) {
 		int offsetIndex = startIndex + 3;
@@ -17,32 +23,86 @@ public class ExtractFile {
 	}
 
 	public static void main(String[] args) throws IOException {
-		String filePath = "src/main/resources/all.txt";
-		retornarDigitos(filePath);
-	}
-	
-	public void checksum() {
+		String filePath = "src/main/resources/file.txt"; 
+		String outPutFilePath = "src/main/resources/fileOut.txt"; 	
 		
+		numerosMap.put("_ | ||_|", 0);
+		numerosMap.put("|  |", 1);
+		numerosMap.put("_  _||_", 2);
+		numerosMap.put("_  _| _|", 3);
+		numerosMap.put("|_|  |", 4);
+		numerosMap.put("_ |_  _|", 5);
+		numerosMap.put("_ |_ |_|", 6);
+		numerosMap.put("_   |  |", 7);
+		numerosMap.put("_ |_||_|", 8);
+		numerosMap.put("_ |_| _|", 9);
+
+		patternMap.put(0, "_ | ||_|");
+		patternMap.put(1, "|  |");
+		patternMap.put(2, "_  _||_");
+		patternMap.put(3, "_  _| _|");
+		patternMap.put(4, "|_|  |");
+		patternMap.put(5, "_ |_  _|");
+		patternMap.put(6, "_ |_ |_|");
+		patternMap.put(7, "_   |  |");
+		patternMap.put(8, "_ |_||_|");
+		patternMap.put(9, "_ |_| _|");
+
+		ArrayList<Integer> digitos = retornarDigitos(filePath);
+		ArrayList<List<Integer>> digitosSegmentados = segmentarDigitos(digitos);
+		for (List<Integer> entrada : digitosSegmentados) {
+			File file = new File(outPutFilePath);
+			FileWriter fr = new FileWriter(file, true);
+			fr.write(testaEntrada(entrada));
+			fr.write("\n");
+			fr.close();
+			;
+		}
+
 	}
 
-	public static ArrayList<String> retornarDigitos(String filePath) {		
+	public static ArrayList<List<Integer>> segmentarDigitos(final List<Integer> digitos) {
+		ArrayList<List<Integer>> segmentos = new ArrayList<List<Integer>>();
+		for (int i = 0; i < digitos.size(); i += 9) {
+			segmentos.add(new ArrayList<Integer>(digitos.subList(i, i + 9)));
+		}
+
+		return segmentos;
+	}
+
+	public static String testaEntrada(List<Integer> entrada) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(entrada.toString().replace(", ", "").replace("-1", "?").substring(1, 10));
+
+		if (entrada.contains(-1)) {
+			return sb.append(" ILL").toString();
+		}
+		if (checksum(entrada)) {
+			return sb.append(" ERR").toString();
+		}
+		return "";
+	}
+
+	public static boolean checksum(List<Integer> entrada) {
+		int multiplicador = 9;
+		int resultado = 0;
+		for (Integer i : entrada) {
+			resultado += i * multiplicador;
+			multiplicador--;
+		}
+
+		if (resultado % 11 == 0) {
+			return true;
+		}
+		return false;
+	}
+
+	public static ArrayList<Integer> retornarDigitos(String filePath) {
 		List<String> rows;
-		ArrayList<String> digitosEmNumeros = new ArrayList<String>();
+		ArrayList<Integer> digitosEmNumeros = new ArrayList<Integer>();
 		try {
-			HashMap<String, Integer> numeros = new HashMap<String, Integer>();
-			numeros.put("_ | ||_|", 0);
-			numeros.put("|  |", 1);
-			numeros.put("_  _||_", 2);
-			numeros.put("_  _| _|", 3);
-			numeros.put("|_|  |", 4);
-			numeros.put("_ |_  _|", 5);
-			numeros.put("_ |_ |_|", 6);
-			numeros.put("_   |  |", 7);
-			numeros.put("_ |_||_|", 8);
-			numeros.put("_ |_| _|", 9);
-			
-			
-			rows = Files.readAllLines(new File(filePath).toPath(), Charset.defaultCharset() );
+			rows = Files.readAllLines(new File(filePath).toPath(), Charset.defaultCharset());
+			rows.removeAll(Arrays.asList("", null));
 			List<String> digitos = new ArrayList<String>();
 
 			for (int linha = 0; linha < rows.size(); linha += linhasPorVez) {
@@ -54,13 +114,14 @@ public class ExtractFile {
 					digitos.add(digito.toString());
 				}
 			}
-			
+
 			for (String string : digitos) {
-				digitosEmNumeros.add(numeros.get(string.trim()) == null ? "?" : numeros.get(string.trim()).toString());
+				digitosEmNumeros.add(numerosMap.get(string.trim()) == null ? -1 : numerosMap.get(string.trim()));
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		System.out.println(digitosEmNumeros);
 		return digitosEmNumeros;
 	}
 }
--- src/test/java/com/teste/extract/tests/ExtractFileTests.java
+++ src/test/java/com/teste/extract/tests/ExtractFileTests.java
@@ -16,37 +16,7 @@ public class ExtractFileTests {
 	 */
 	@Test
 	public void testAll() {
-		String allError = diretorio + "all.txt";
-		assertEquals("1234?678?",  String.join("",ExtractFile.retornarDigitos(allError)));
-	}
-	
-	/**
-	 * Teste numero 4
-	 * @author marina
-	 */
-	@Test
-	public void testFour() {
-		String numberFour = diretorio + "4.txt";
-		assertEquals("444444444", String.join("",ExtractFile.retornarDigitos(numberFour)));
-	}
-	
-	/**
-	 * Teste numero 7
-	 * @author marina
-	 */
-	@Test
-	public void testSeven() {
-		String numberSeven = diretorio + "7.txt";
-		assertEquals("777777777", String.join("",ExtractFile.retornarDigitos(numberSeven)));
-	}
-	
-	/**
-	 * Teste numero 3
-	 * @author marina
-	 */
-	@Test
-	public void testThree() {
-		String numberThree = diretorio + "3.txt";
-		assertEquals("333333333",  String.join("",ExtractFile.retornarDigitos(numberThree)));
+		String allError = diretorio + "file.txt";
+		assertEquals("123456789", ExtractFile.retornarDigitos(allError).toString().replace(", ", "").replace("-1", "?").substring(1, 10));
 	}
 }
